faac6ecdc7f55924abdcf7e2005a3f928a886d07
YOGESSH V <40272613+Yogessh3@users.noreply.github.com>
2021-01-02 11:13:59 +0530

    Band Name Generator

---
+++ day1_project_BandNameGenerator.py
@@ -0,0 +1,9 @@
+'''
+Band Name Generator: Create a band name generator
+'''
+print("Welcome to the Band Name Generator")
+city=input("Which city you grew up in ?\n")
+pet_name=input("What's your pet's name ?\n")
+band_name=city+" "+pet_name
+print("Your band name could be "+band_name)
+
